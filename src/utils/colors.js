4ceaa89398d7a78226e897cfdc49b621c049ca7b
Carlos Moreno <cmoreno@clmoreno.com>
2018-06-20 20:44:04 -0600

    Add utils
    - Added colors colection

---
+++ src/utils/colors.js
@@ -0,0 +1,40 @@
+const colors = {
+  blueGrey: {
+    main: '#34495e',
+    light: '#60748b',
+    dark: '#092234',
+    text: '#ffffff'
+  },
+  yellow: {
+    main: '#e5b25d',
+    light: '#ffe48c',
+    dark: '#b0832f',
+    text: '#ffffff'
+  },
+  blue: {
+    main: '#2980b9',
+    light: '#66afec',
+    dark: '#005489',
+    text: '#ffffff'
+  },
+  turquoise: {
+    main: '#17a589',
+    light: '#5bd7b9',
+    dark: '#00755c',
+    text: '#ffffff'
+  },
+  white: {
+    main: '#ffffff',
+    light: '#ffffff',
+    dark: '#cccccc',
+    text: '#000000'
+  },
+  red: {
+    main: '#cd6155',
+    light: '#ff9182',
+    dark: '#97322c',
+    text: '#ffffff'
+  }
+};
+
+export default colors;
